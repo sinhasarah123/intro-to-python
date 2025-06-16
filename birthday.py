26d40a3cd5af212c72bb5133d897433b8ceaf85f
sarahsinha123 <sinhasarah47@gmail.com>
2025-06-16 19:45:36 +0530

    birthdays

--- birthday.py
+++ birthday.py
@@ -1,2 +1,11 @@
-print("hi ,welcome to printing birthdays!")
-print(input("what is your birthday?:"))
+print("birthdays of 5 friends \n")
+a="6-6-95"
+print("sarah-",a)
+b="9-6-45"
+print("kiah-",b)
+c="4-11-98"
+print("suki-",c)
+d="17-5-98"
+print("sira-",d)
+e="6-7-98"
+print("apparna-",e)
