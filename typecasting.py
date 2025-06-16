87d29b659dd44a1e2f0ba60d3538de504d3aed48
sarahsinha123 <sinhasarah47@gmail.com>
2025-06-16 19:22:17 +0530

    typecasting

---
+++ typecasting.py
@@ -0,0 +1,22 @@
+name="sarah"
+age=10
+is_student=True
+
+print("Name:",name)
+print("age:",age)
+print("is student:",is_student)
+
+print(" \n after typecasting" )
+name=str(name)
+age=str(age)
+is_student=str(is_student)
+
+print("Name:",name)
+print("age:",age)
+print("is student:",is_student)
+
+print("\n data types of the variables:")
+print("data type of name:",type(name))
+print("data type of age:",type(age))
+print("data type of is_student:",type(is_student))
+
