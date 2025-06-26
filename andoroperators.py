160593c45656eb628df5ce04f9fdc3230aec5704
sarahsinha123 <sinhasarah47@gmail.com>
2025-06-26 16:30:43 +0530

    and or

---
+++ andoroperators.py
@@ -0,0 +1,16 @@
+a=10
+b=12
+c=0
+if a and b and c:
+    print("All values are boolean values True ") 
+else:
+    print("At least one value is boolean value False")
+
+a= 10
+b= -10
+c= 0
+if a>0 or b>0 or c>0:
+    print("At least one value greater than 0")
+else:
+    print("All values are less than or equal to 0")
+
