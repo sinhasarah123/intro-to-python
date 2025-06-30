1a3529a59629473894a60ebf0546345ae25d8c7f
sarahsinha123 <sinhasarah47@gmail.com>
2025-06-30 19:04:11 +0530

    identityoperation

---
+++ identityoperators.py
@@ -0,0 +1,21 @@
+x = 10
+if(type(x)==int):
+    print("x is an integer")
+else:
+    print("x is not an integer")
+
+y = 2.26
+if(type(y)==float):
+    print("y is a float")
+else:
+    print("y is not a float")
+
+x=10
+y=20
+if(x > y):
+    print("x is greater than y")
+elif(x < y):
+    print("x is less than y")
+else:
+    print("x is equal to y")
+
