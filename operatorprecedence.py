5e1e6371d8dbbc0556dde5371b4dea2a83adbccd
sarahsinha123 <sinhasarah47@gmail.com>
2025-07-03 16:24:05 +0530

    operatorprecedence

---
+++ operatorprecedence.py
@@ -0,0 +1,15 @@
+v=5
+r=10
+z=3
+t=2
+x=1
+a= v + r * z - t / x
+print("The result of the calculation is:", a)
+
+name="John"
+age=30
+if name == "John" or name == "Jane":
+    if age >= 18:
+        print("Hello, welcome")
+    else:
+        print("goodbye!!")
