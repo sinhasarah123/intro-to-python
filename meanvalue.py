0259572871a99c3d14e2a7cc2ab94c29156adc07
sarahsinha123 <sinhasarah47@gmail.com>
2025-07-03 16:41:13 +0530

    means

---
+++ meanvalue.py
@@ -0,0 +1,11 @@
+mean1 = 38
+wrong_number = 36
+correct_number = 56
+total_number = 5
+sum = mean1*total_number
+print("The sum of the numbers is", sum)
+num2 = sum - wrong_number+correct_number
+print("The sum of the numbers after correction is", num2)
+mean2 = num2 / total_number
+print("the correct mean is:",mean2)
+
